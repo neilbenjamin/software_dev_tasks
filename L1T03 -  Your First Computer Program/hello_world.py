be0bd0901de4853420fb078f889887f0d1987ee3
Neil <63252317+neilbenjamin@users.noreply.github.com>
2025-02-24 23:41:49 +0200

    Add files via upload

---
+++ L1T03 -  Your First Computer Program/hello_world.py
@@ -0,0 +1,17 @@
+# Pseudo-Code Solution
+
+#Ask the user to input their name using the input command
+#Store this input (argument) in a variable named 'name'
+#Display the user's name with the print command calling the name variable
+#Ask the user to input their age using the input command
+#Store this input or argument in a new variable named 'age'
+#Display the user's age with the print command calling the age variable. 
+#Finally execute the print=("Hello World!") statement as the last execution.
+
+#Code
+
+name = input ("What is your name?")
+print(name)
+age = input("What is your age?")
+print(age)
+print("Hello World!")
---
+++ L1T03 -  Your First Computer Program/menu.py
@@ -0,0 +1,18 @@
+# Task
+
+# Ask the user to input their 3 favourite foods from a menu, 
+# Use consecutive input commands sequence and save each input to a new variable labelled item1, item2 and item3 respectively. 
+# Create a generic print statement confirming their inputs and then
+# Print each variable on a new line.
+
+#Code
+
+print("Please order your 3 favourite meals from the menu.")
+item1 = input("What is your first choice?")
+item2 = input("What is your second choice?")
+item3 = input("What is your third choice?")
+print("Order confirmed. You have ordered:")
+print(item1)
+print(item2)
+print(item3)
+
